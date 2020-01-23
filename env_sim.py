bf63758997dbf846f0f84d16c1998234e88f29cd
ay20-5-1994@hotmail.com <ay02-5-1994@hotmail.com>
2020-01-24 02:45:08 +0300

    Data from file

--- env_sim.py
+++ env_sim.py
@@ -8,7 +8,7 @@ import math
 import os
 import gym
 from tqdm import trange
-import subprocess,signal
+import subprocess, signal
 from threading import Thread
 
 p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
@@ -35,19 +35,18 @@ if ID != -1:
 else:
     sys.exit("Error")
 
+start = 0.0
 
-
-start=0.0
 
 class Robot:
 
     def __init__(self, ID, num):
         self.ID = ID
         self.num = num
-        self.angles=[np.pi/2.0,0.0,-np.pi/2.0,-np.pi/2.0]
-        self.dir=0
-        self.r=0.0975
-        self.l=0.381
+        self.angles = [np.pi, np.pi / 2.0, 0.0,3*np.pi / 2.0]
+        self.dir = 1
+        self.r = 0.0975
+        self.l = 0.55
 
         # motors
         self.left_handle = self.get_handle("motor%d_left" % (self.num))
@@ -57,6 +56,7 @@ class Robot:
         self.robot_handle = self.get_handle("R%d" % (self.num))
         (code, pose) = vrep.simxGetObjectPosition(self.ID, self.robot_handle, -1, const_v.simx_opmode_streaming)
         (code, rot) = vrep.simxGetObjectOrientation(self.ID, self.robot_handle, -1, const_v.simx_opmode_streaming)
+        self.points = []
         time.sleep(0.1)
 
     def get_handle(self, name):
@@ -72,39 +72,48 @@ class Robot:
 
     def get_phi(self):
         (code, rot) = vrep.simxGetObjectOrientation(self.ID, self.robot_handle, -1, const_v.simx_opmode_buffer)
-        phi=rot[2]
+        phi = rot[2]
         return phi
-
 
     def position_xy(self):
         pose = self.get_position()
-        time.sleep(0.1)
-        x = (pose[0] + 4.5)
-        y = (pose[1] + 4.5)
-        return x,y
+        x = (pose[0] + 4.5)+0.1
+        y = (pose[1] + 4.5)+0.1
+        return x, y
 
-    def rotate(self,angle):
-        phi=self.get_phi()
-        if phi<0:
-            phi+=2*np.pi
-        if phi<angle:
-            tmp=angle
-            angle=phi
-            phi=tmp
-        if(phi-angle<angle+(2*np.pi-phi)):
-            err=-(phi-angle)
+    def rotate(self, dir):
+        self.dir=dir
+        angle = self.angles[int(dir)]
+        phi = self.get_phi()
+        if phi < 0:
+            phi += 2 * np.pi
+        if phi < angle:
+            tmp = angle
+            angle = phi
+            phi = tmp
+        if phi - angle < angle + (2 * np.pi - phi):
+            err = -(phi - angle)
         else:
-            err=angle+2*np.pi-phi
-        w=err*self.l/(4.0*self.r)
-        if abs(w)>0.05:
-            self.move_robot(-w,w)
+            err = angle + 2 * np.pi - phi
+        global start
+        dt = 1.0 - (time.time() - start)
+        w = err * self.l / (4.0 * self.r)
+        if abs(w) > 0.1:
+            self.move_robot(-w, w)
 
-    def goto_xy(self,x,y):
-        xr,yr=self.position_xy()
-        w=(x-yr)/(2*self.r)
-        self.move_robot(w/2,w/2)
-        if w<0.1:
-            self.move_robot(0.0,0.0)
+    def goto_xy(self, x, y):
+        xr, yr = self.position_xy()
+        if self.dir == 0 or self.dir == 2:
+            dis = x - xr
+        else:
+            dis = y - yr
+        # print(x,xr,y,yr)
+        global start
+        dt = 1.0 - (time.time() - start)
+        w = dis / (self.r)
+        self.move_robot(w / 2, w / 2)
+        if abs(w) < 0.05:
+            self.move_robot(0.0, 0.0)
 
     def get_image(self, cam_handle):
         (code, res, im) = vrep.simxGetVisionSensorImage(self.ID, cam_handle, 0, const_v.simx_opmode_buffer)
@@ -117,39 +126,39 @@ class Robot:
         code = vrep.simxSetJointTargetVelocity(self.ID, self.left_handle, v_left, const_v.simx_opmode_streaming)
         code = vrep.simxSetJointTargetVelocity(self.ID, self.right_handle, v_right, const_v.simx_opmode_streaming)
 
-points=[[0.0,0.0,np.pi/2],
-        [0.25,0.0,np.pi/2],
-        [1.0,0.0,np.pi/2],
-        [1.75,0.0,np.pi/2],
-        [2.0,0.0,np.pi/2],
-        [2.0,0.0,np.pi/2],
-        [2.0,0.0,np.pi/2],
-        [2.0,0.0,np.pi/2],
-        [2.0,0.0,0.0],
-        [2.0,0.0,0.0],
-        [2.0,0.0,0.0],
-        [2.0,0.0,0.0],
-        [2.0,0.0,0.0]]
+
+file = open("/home/ali/PycharmProjects/Multi/in.txt", "r")
 num_robots = 5
 R = []
 for i in range(num_robots):
     R.append(Robot(ID, i + 1))
 
-start=time.time()
-for p in points:
-    start=time.time()
-    for r in R:
-        r.goto_xy(p[0], p[1])
-        r.rotate(p[2])
-    total_time=time.time()-start
-    time.sleep(1.0-total_time)
+# while line!=None:
+robot_points = []
+for line in file.read().split('\n'):
+    numbers = []
+    if len(line)==0:
+        break
+    for y in line.split(' '):
+        if y:
+            numbers.append(float(y))
+    for i in range(num_robots):
+        R[i].points.append(np.array([numbers[4 * i + 1]/1000.0,numbers[4 * i + 2]/1000.0,int(numbers[4*i+3])]))
+d=[]
+for i in range(num_robots):
+    d.append(R[i].points[0][2])
 
+for p in range(len(R[0].points)):
+    start = time.time()
+    for i in range(5):
+        if abs(R[i].points[p][2]-d[i])>0.1:
+            # print("rot")
+            R[i].rotate(R[i].points[p][2])
+        else:
+            R[i].goto_xy(i + R[i].points[p][0], R[i].points[p][1])
+        d[i] = R[i].points[p][2]
+    total_time = time.time() - start
+    time.sleep(1.0 - total_time)
 for r in R:
     r.move_robot(0.0,0.0)
-    time.sleep(0.1)
-#
-# total_time_steps=10
-# for _ in range(total_time_steps):
-#     for i in range(num_robots):
-#         R[i].goto_xy(i,i)
-#         time.sleep(0.5)
+# time.sleep(10)
